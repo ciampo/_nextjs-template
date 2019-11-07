aaaa3ef0e80a8ca8e286a7c74448e630b239750d
Marco Ciampini <marcociampini@toaster.co>
2019-11-07 12:35:05 +0800

    do not purgecss / cssnano when in dev mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+const dev = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   plugins: [
     require('stylelint'),
     require('postcss-easy-import'),
     require('tailwindcss'),
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./pages/**/*.{js,tsx}', './components/**/*.{js,tsx}'],
-      defaultExtractor: (content) => content.match(/[\w-:/]+(?<!:)/g) || [],
-    }),
+    dev
+      ? null
+      : require('@fullhuman/postcss-purgecss')({
+          content: ['./pages/**/*.{js,tsx}', './components/**/*.{js,tsx}'],
+          defaultExtractor: (content) => content.match(/[\w-:/]+(?<!:)/g) || [],
+        }),
     require('autoprefixer'),
-    require('cssnano'),
+    dev ? null : require('cssnano'),
     require('postcss-reporter')({ clearReportedMessages: true }),
-  ],
+  ].filter((plugin) => plugin !== null),
 };
