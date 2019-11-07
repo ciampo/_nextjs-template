436b1f209465c1e76086995221ecadcd6bc3cdaa
Marco Ciampini <marcociampini@toaster.co>
2019-11-07 15:23:24 +0800

    custom _document.tsx allows to inject script tag to add "js" class to HTML element

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from 'next/document';
+
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <script>{`document.documentElement.classList.add('js-app')`}</script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
