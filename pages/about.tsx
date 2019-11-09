ac7748ed1ea1cbdc43c88c94863567c2f46bef39
Marco Ciampini <marcociampini@toaster.co>
2019-11-09 12:32:00 +0800

    expriment with contentful data in the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,26 +1,48 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { NextComponentType } from 'next';
 import Head from 'next/head';
 
 import DefaultPageTransitionWrapper from '../components/page-transition-wrappers/default';
 
-const About: React.FC<{}> = () => {
-  const [name] = useState('Example');
+type AboutPageProps = {
+  title: string;
+};
 
-  function aboutName(name: string): string {
-    return `About ${name}`;
+type AboutAPIs = {
+  title: string;
+  id: string;
+  bio: object;
+};
+
+const About: NextComponentType<{}, {}, AboutPageProps> = ({ title }) => {
+  const [exampleState] = useState('Example');
+
+  function aboutFullTitle(exampleState: string): string {
+    return `${exampleState} — ${title}`;
   }
 
   return (
     <>
       <Head>
-        <title>{aboutName(name)}</title>
+        <title>{aboutFullTitle(exampleState)}</title>
       </Head>
 
       <DefaultPageTransitionWrapper>
-        <div>{aboutName(name)}</div>
+        <div>{aboutFullTitle(exampleState)}</div>
       </DefaultPageTransitionWrapper>
     </>
   );
+};
+
+About.getInitialProps = async (): Promise<AboutPageProps> => {
+  const about: AboutAPIs[] = await import('../data/about.json').then((m) => m.default);
+
+  return { title: about[0].title };
+};
+
+About.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default About;
